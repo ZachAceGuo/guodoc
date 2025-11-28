9374b736b5260bacd7c398d55af5da2df01409c3
GuoZach <11770695+zachkingguo@user.noreply.gitee.com>
2025-11-28 16:23:59 +0800

    feat(navbar): 更新导航栏配置并新增面试题目内容

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,24 +3,30 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    "portfolio",
+    // "portfolio",
+    // {
+    //   text: "案例",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
+    // {
+    //   text: "文档",
+    //   icon: "book",
+    //   prefix: "guide/",
+    //   children: "structure",
+    // },
     {
-      text: "案例",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文档",
+      text: "面试每日题目",
       icon: "book",
-      prefix: "guide/",
+      prefix: "mianshi/",
       children: "structure",
     },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
-    },
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
+    // },
   ],
 });
